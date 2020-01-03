d3d2537ba7cffaac796ef074661d427bc1ab4190
jesusdc99 <mbp@MBP.local>
2020-01-04 00:04:00 +0100

    Actualizadas variables de la config Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCWZeuzDQ_Yb5a5d-3_n3D1GkaAtwkHtFk",
+    authDomain: "horario-tea.firebaseapp.com",
+    databaseURL: "https://horario-tea.firebaseio.com",
+    projectId: "horario-tea",
+    storageBucket: "horario-tea.appspot.com"
+  },
+  collection: "card"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCWZeuzDQ_Yb5a5d-3_n3D1GkaAtwkHtFk",
+    authDomain: "horario-tea.firebaseapp.com",
+    databaseURL: "https://horario-tea.firebaseio.com",
+    projectId: "horario-tea",
+    storageBucket: "horario-tea.appspot.com"
+  },
+  collection: "card"
 };
 
 /*
