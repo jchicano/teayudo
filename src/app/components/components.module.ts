a4a48ef4a1d73db8e00cfbba878b55f86f88a6c2
jesusdc99 <mbp@MBP.local>
2020-01-21 22:19:53 +0100

    Añadido componente SpinnerComponent

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,3 +1,5 @@
+import { IonicModule } from '@ionic/angular';
+import { SpinnerComponent } from './spinner/spinner.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfirmCheckboxComponent } from './confirm-checkbox/confirm-checkbox.component';
@@ -5,10 +7,11 @@ import { ConfirmCheckboxComponent } from './confirm-checkbox/confirm-checkbox.co
 // NOTE https://youtu.be/za5NaFavux4
 
 @NgModule({
-  declarations: [ConfirmCheckboxComponent],
+  declarations: [ConfirmCheckboxComponent, SpinnerComponent],
   imports: [
     CommonModule,
+    IonicModule // NOTE https://stackoverflow.com/a/54387068/10387022
   ],
-  exports: [ConfirmCheckboxComponent]
+  exports: [ConfirmCheckboxComponent, SpinnerComponent]
 })
 export class ComponentsModule { }
