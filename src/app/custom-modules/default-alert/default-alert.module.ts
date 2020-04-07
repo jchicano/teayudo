39b22967fe08fdf25cd447304265bc9ec5713225
jesusdc99 <36838479+jesusdc99@users.noreply.github.com>
2020-04-07 17:34:25 +0200

    Modificado y añadido alert

--- src/app/custom-modules/default-alert/default-alert.module.ts
+++ src/app/custom-modules/default-alert/default-alert.module.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -11,7 +12,8 @@ import { CommonModule } from '@angular/common';
 export class DefaultAlertModule {
 
   constructor(
-    private alertController: AlertController
+    private alertController: AlertController,
+    private router: Router
   ) { }
 
   async show(title: string, subtitle: string, msg: string, buttons: string[] = ['OK']) {
@@ -19,7 +21,35 @@ export class DefaultAlertModule {
       header: title,
       subHeader: subtitle,
       message: msg,
-      buttons: buttons
+      backdropDismiss: false,
+      buttons: buttons,
+    });
+
+    await alert.present();
+  }
+
+  async showNavigateOnOk(title: string, subtitle: string, msg: string, url: string) {
+    const alert = await this.alertController.create({
+      header: title,
+      subHeader: subtitle,
+      message: msg,
+      backdropDismiss: false,
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Dialog Cancel');
+          }
+        }, {
+          text: 'OK',
+          handler: () => {
+            console.log('Dialog Okay');
+            this.router.navigate([url]);
+          }
+        }
+      ]
     });
 
     await alert.present();
